c9015fe685a6fe1c59a76f58533cb7a917837dd7
PeerawatAltoTechCourse <peerawat.y@ku.th>
2023-06-19 23:59:10 +0700

    Process2

--- basic_oop.py
+++ basic_oop.py
@@ -1,30 +1,33 @@
 #project salary man
 #intruct how to work
 class Employee: #class created
-    pass
+    def detail(self,engname,engsalary,engmajor):
+        self.name = engname
+        self.salary = engsalary
+        self.major = engmajor 
+        print("the engineer name :  {}".format(self.name))
+        print("and his salary is {}".format(self.salary)) 
+        print("graduated {}".format(self.major))
 
 #object created
 #working
 emp1 = Employee()
+emp1.detail("Peerawat",30000,"Power")
 
-# Attribute
-# Create attribute using "self.name" should be meaningful 
-# Ex self.age means employee age
+emp2 = Employee()
+emp2.detail("Messi",500000,"Data management")
 
 
 
-# Method is mechanism of behavior
-# Method is to create "function(def)" to work 
+#[syntax] def detail(self,name,salary) , we add parameters to () of the function
+# the name and salary means you need to put them on to have successful using the function
+# 5th,6th lines means attribute is a parameter we define in finction() 
+# the attribute parameters name must be the same as parameter we put in the funtion() 
+# and already define in the function() that authorized  the attributes 
+# 12th-15th we put parameter info into the object that working by the function
+# we can have employee info more than one 
 
-# TO create method : def FuncName(param)
-# def getName(self):
-#     return self.name
 
-# TO call :
-# ObjName.getName()
-
-# Keyword "self" is to identify which "Object" we working with 
-# such as object's attribute definition 
 
 
 
