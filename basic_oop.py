94f4d152fe951a44bd9ef33e26cb1fed20e74145
PeerawatAltoTechCourse <peerawat.y@ku.th>
2023-06-19 21:51:12 +0700

    Project OOP

---
+++ basic_oop.py
@@ -0,0 +1,30 @@
+#project salary man
+#intruct how to work
+class Employee: #class created
+    pass
+
+#object created
+#working
+emp1 = Employee()
+
+# Attribute
+# Create attribute using "self.name" should be meaningful 
+# Ex self.age means employee age
+
+
+
+# Method is mechanism of behavior
+# Method is to create "function(def)" to work 
+
+# TO create method : def FuncName(param)
+# def getName(self):
+#     return self.name
+
+# TO call :
+# ObjName.getName()
+
+# Keyword "self" is to identify which "Object" we working with 
+# such as object's attribute definition 
+
+
+
